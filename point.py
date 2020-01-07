d1ef44a960a60e278799c739b6185f2a4fabf2d2
W1nson <40487548+W1nson@users.noreply.github.com>
2020-01-07 01:48:10 -0800

    Add files via upload

---
+++ point.py
@@ -0,0 +1,16 @@
+
+
+import random as r
+
+class point:
+
+    def __init__( label):
+        self.x = r.randrange(-250, 250)
+        self.y = r.randrange(-250, 250)
+        if x > y:
+            self.label = 1
+        else:
+            self.labe = -1
+
+
+    
